67bd40f9a0439e93531de705caa80a7a1d1c5908
1terator <kovalexnader13@gmail.com>
2023-08-09 10:53:42 +0300

    add project image to admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Project
+from .models import Project, ProjectImage
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("id",)
+    list_display = (
+        "preview",
+        "title",
+        "created_at",
+    )
 
     def preview(self, obj):
         if obj.logo:
@@ -14,4 +18,18 @@ class ProjectAdmin(admin.ModelAdmin):
             return "[no image]"
 
 
+class ProjectImageAdmin(admin.ModelAdmin):
+    list_display = (
+        "project",
+        "preview",
+    )
+
+    def preview(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{}" height="80">'.format(obj.image.url))
+        else:
+            return "[no image]"
+
+
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectImage, ProjectImageAdmin)
--- projects/serializers.py
+++ projects/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import fields, serializers
 
-from projects.models import Project
+from projects.models import Project, ProjectImage
+
+
+class ProjectImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectImage
+        fields = ("id", "image")
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    images = ProjectImageSerializer(read_only=True, many=True)
+
     class Meta:
         model = Project
-        fields = ()
+        fields = ("id", "name", "title", "logo", "images", "description", "is_active", "created_at")
