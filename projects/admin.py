ce92066ac9e1f26786a2d2d20fab4932c3afac5a
1terator <kovalexnader13@gmail.com>
2023-08-09 10:28:38 +0300

    add project to admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Project
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "preview",
+        "title",
+        "created_at",
+    )
+
+    def preview(self, obj):
+        if obj.logo:
+            return mark_safe('<img src="{}" height="80">'.format(obj.logo.url))
+        else:
+            return "[no image]"
+
+
+admin.site.register(Project, ProjectAdmin)
