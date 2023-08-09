51881c1290408ebc6e141688ed14b652dfbae1f6
1terator <kovalexnader13@gmail.com>
2023-08-09 12:57:05 +0300

    add project image to admin

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
@@ -29,3 +31,6 @@ urlpatterns = [
     path("api/v1/projects/", include(projects_router.urls)),
     path("api/v1/feedback/", EmailFormView.as_view(), name="feedback"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
