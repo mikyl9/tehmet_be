5c16981fb0fe3f12743ed9bacfced94a42787e6c
1terator <kovalexnader13@gmail.com>
2023-08-10 14:34:35 +0300

    fix media files delete nginx

--- core/urls.py
+++ core/urls.py
@@ -32,5 +32,4 @@ urlpatterns = [
     path("api/v1/feedback/", EmailFormView.as_view(), name="feedback"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
